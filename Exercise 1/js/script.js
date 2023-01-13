30518bc8f1e0d4a3b17091989f1898084625715d
Alexander MacKinnon <alexmackinnon55@gmail.com>
2023-01-12 23:47:52 -0500

    Create paddle bounce and speed variations

--- Exercise 1/js/script.js
+++ Exercise 1/js/script.js
@@ -18,11 +18,19 @@ let secondPaddlePos = 250;
 
 // Ball Properties
 let ballPosX, ballPosY;
-let ballSize = 15;
-let ballSpeedX = 8; // Speed of the shape
-let ballSpeedY = 4; // Speed of the shape
+let ballSize = 10;
 let ballTrajectoryX = 1; // Left or Right
 let ballTrajectoryY = 1; // Top to Bottom
+let ballSpeedY = {
+  active: 0,
+  max: 10,
+  min: 0,
+};
+let ballSpeedX = {
+  active: 8,
+  max: 15,
+  min: 5,
+};
 
 /* ============ Preload ============ */
 function preload() {}
@@ -63,17 +71,88 @@ function draw() {
 
   paddleMove();
 
-  // Update the position of the shape
-  ballPosX = ballPosX + ballSpeedX * ballTrajectoryX;
-  ballPosY = ballPosY + ballSpeedY * ballTrajectoryY;
+  // Find Paddle 1 Edges
+  let firstPaddleTop = firstPaddlePos - paddleHeight / 2;
+  let firstPaddleRight = width - width + 20 + paddleWidth;
+  let firstPaddleBottom = firstPaddlePos + paddleHeight / 2;
+  let firstPaddleLeft = width - width + 20 + paddleWidth - paddleWidth;
 
-  // Test to see if the shape exceeds the boundaries of the screen
-  // If it does, reverse its direction by multiplying by -1
-  if (ballPosX > width - ballSize || ballPosX < ballSize) {
+  // Find Paddle 2 Edges
+  let secondPaddleTop = secondPaddlePos - paddleHeight / 2;
+  let secondPaddleRight = width - 20 + paddleWidth;
+  let secondPaddleBottom = secondPaddlePos + paddleHeight / 2;
+  let secondPaddleLeft = width - 20 + paddleWidth - paddleWidth;
+
+  // Update the position of the shape
+  ballPosX = ballPosX + ballSpeedX.active * ballTrajectoryX;
+  ballPosY = ballPosY + ballSpeedY.active * ballTrajectoryY;
+
+  // Ball bounces horizontally if it hits a paddle
+  if (
+    ballPosY >= firstPaddleTop &&
+    ballPosY <= firstPaddleBottom &&
+    ballPosX <= firstPaddleRight &&
+    ballPosX >= firstPaddleLeft
+  ) {
+    if (ballSpeedY.active <= ballSpeedY.min) {
+      ballSpeedY.active = ballSpeedY.min + 2;
+    }
+    if (ballSpeedY.active >= ballSpeedY.max) {
+      ballSpeedY.active = ballSpeedY.max - 2;
+    }
+    if (ballSpeedX.active <= ballSpeedX.min) {
+      ballSpeedY.active = ballSpeedY.min + 2;
+    }
+    if (ballSpeedX.active >= ballSpeedX.max) {
+      ballSpeedX.active = ballSpeedX.max - 2;
+    }
     ballTrajectoryX *= -1;
+    ballSpeedX.active = random(ballSpeedX.active - 2, ballSpeedX.active + 2);
+    ballSpeedY.active = random(ballSpeedY.active - 2, ballSpeedY.active + 2);
+    if (ballPosY < firstPaddlePos) {
+      ballTrajectoryY = -1;
+    } else {
+      ballTrajectoryY = 1;
+    }
   }
+
+  if (
+    ballPosY >= secondPaddleTop &&
+    ballPosY <= secondPaddleBottom &&
+    ballPosX <= secondPaddleRight &&
+    ballPosX >= secondPaddleLeft
+  ) {
+    if (ballSpeedY.active <= ballSpeedY.min) {
+      ballSpeedY.active = ballSpeedY.min + 2;
+    }
+    if (ballSpeedY.active >= ballSpeedY.max) {
+      ballSpeedY.active = ballSpeedY.max - 2;
+    }
+    if (ballSpeedX.active <= ballSpeedX.min) {
+      ballSpeedX.active = ballSpeedX.min + 2;
+    }
+    if (ballSpeedY.active >= ballSpeedX.max) {
+      ballSpeedX.active = ballSpeedX.max - 2;
+    }
+    ballTrajectoryX *= -1;
+    ballSpeedX.active = random(ballSpeedX.active - 2, ballSpeedX.active + 2);
+    ballSpeedY.active = random(ballSpeedY.active - 2, ballSpeedY.active + 2);
+    if (ballPosY < secondPaddlePos) {
+      ballTrajectoryY = -1;
+    } else {
+      ballTrajectoryY = 1;
+    }
+  }
+
+  // Ball bounces vertically if hits upper or lower wall
   if (ballPosY > height - ballSize || ballPosY < ballSize) {
     ballTrajectoryY *= -1;
+  }
+
+  // Ball stops if it hits left or right wall
+  if (ballPosX > width - ballSize || ballPosX < ballSize) {
+    ballTrajectoryX = 0;
+    ballTrajectoryY = 0;
   }
 
   // Ball
@@ -115,18 +194,6 @@ function paddleMove() {
       secondPaddlePos += 5;
     }
   }
-
-  // Find Paddle 1 Edges
-  let firstPaddleTop = firstPaddlePos - paddleHeight / 2;
-  let firstPaddleRight = width - width + 20 + paddleWidth;
-  let firstPaddleBottom = firstPaddlePos + paddleHeight / 2;
-  let firstPaddleLeft = width - width + 20 + paddleWidth - paddleWidth;
-
-  // Find Paddle 2 Edges
-  let secondPaddleTop = secondPaddlePos - paddleHeight / 2;
-  let secondPaddleRight = width - 20 + paddleWidth;
-  let secondPaddleBottom = secondPaddlePos + paddleHeight / 2;
-  let secondPaddleLeft = width - 20 + paddleWidth - paddleWidth;
 }
 
 /* ============ Start Game ============ */
