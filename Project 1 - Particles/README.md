bf52c4289954b056fd7f700a949f266f944a48ba
Alexander MacKinnon <alexmackinnon55@gmail.com>
2023-02-07 22:23:07 -0500

    Release final product of project

--- Project 1 - Particles/README.md
+++ Project 1 - Particles/README.md
@@ -1,3 +1,14 @@
 # Particles Project - Aggresive & Dangerous
 
-Particle system based on the prompt "Aggresive & Dangerous".
+About the Prompt
+
+- For this project, it was required to create a particle system based on a given prompt, which in this case was "Aggresive & Dangerous"
+- To respond to this prompt through visualization, I wanted to create something that looks alarming, something "in-your-face".
+- I made a system that is accentued by bright red colors to illustrate this sense of urgency, of dangerousness. I also made a system that reacts very fast so that it can be perceived as aggressive.
+
+About the Particle System
+
+- The particle system is shown on a grid of rounded squares that each loop through a color and scale animation cycle.
+- Upon startup, a square is selected at random on each frame and given an aggressive attribute, functioning through a boolean variable.
+- Once a square turns aggressive, it starts growing and fading to a red color until reaching its max intensity, which it then sets back to its original form and goes through the animation process again.
+- After a few seconds of viewing, each square will be aggressive, creating a visually interesting loop of animated particles.
--- Project 1 - Particles/js/script.js
+++ Project 1 - Particles/js/script.js
@@ -4,41 +4,102 @@ by Alexander MacKinnon
 */
 
 // --------------- Variables  ---------------
-let ball = [];
-let ballInitSize = 25;
+
+// Array that will store our particles
+let squares = [];
+
+// Array that holds the aggressive square
+let aggressiveSquare;
+
+// Array that stores threshold for the lerped color for fade animation
+let colorFade;
+
+// Set initial square size
+let squareInitSize = 20;
+
+// Set number of columns & rows
+let ncols = 15;
+let nrows = 15;
+
+function preload() {
+  // Sound
+  sound = loadSound("assets/sounds/static.wav");
+}
 
 // --------------- Set-up Function  ---------------
 function setup() {
-  let ncols = 17;
-  let nrows = 17;
+  // Create Canvas
   createCanvas(500, 500);
+
+  // Create Grid
   for (let i = 0; i < ncols; i++) {
     for (let j = 0; j < nrows; j++) {
       let x = (j * width) / (ncols * 1.0);
       let y = (i * height) / (nrows * 1.0);
-      ball[i * ncols + j] = new Particle(25, x, y);
+      squares[i * ncols + j] = new Particle(squareInitSize, x, y);
     }
   }
 }
 
 // --------------- Draw Function  ---------------
 function draw() {
+  // Draw background
   background(0);
-  for (let i = 0; i < ball.length; i++) {
-    ball[i].display();
+
+  // Pick a square at random in our array to turn aggressive
+  aggressiveSquare = floor(random(0, nrows * nrows));
+
+  // Draw grid from squares in our array
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].display();
+    squares[aggressiveSquare].turnAggressive();
   }
 }
 
 // --------------- Particle Object ---------------
 class Particle {
-  constructor(pSize, x, y) {
-    this.x = x + pSize;
-    this.y = y + pSize;
-    this.diameter = pSize;
+  // Constructor properties
+  constructor(squareSize, x, y) {
+    this.x = x + (squareSize - squareSize / 5);
+    this.y = y + (squareSize - squareSize / 5);
+    this.size = squareSize;
+    this.aggressive = false;
+    this.intensity = 0;
   }
+  // Display conditions
   display() {
-    fill(255);
-    ellipseMode(CENTER);
-    ellipse(this.x, this.y, this.diameter);
+    // Colors are stored here
+    let colors = {
+      gray: color(30, 30, 30),
+      red: color(250, 70, 55),
+    };
+    // If the square is given the aggressive attribute...
+    if (this.aggressive) {
+      // And as long as it is under a given intensity...
+      if (this.intensity < 2) {
+        // Rise the intensity
+        this.intensity += 0.1;
+        // Create a fade animation with the intensity using lerp
+        colorFade = lerpColor(colors.gray, colors.red, this.intensity);
+        fill(colorFade);
+      } else {
+        // Reset intensity to 0 and restart the square animation loop
+        this.intensity = 0;
+      }
+      // If not aggressive, square appears as gray
+    } else {
+      fill(colors.gray);
+    }
+    // Square properties go here
+    noStroke();
+    rectMode(CENTER);
+    // Draw square, with size varying depending on its current intensity
+    square(this.x, this.y, this.size + this.intensity * 5, 5);
+  }
+  // Selects every frame a square to turn "aggressive"
+  turnAggressive() {
+    if (frameCount % 1 == 0) {
+      this.aggressive = true;
+    }
   }
 }
