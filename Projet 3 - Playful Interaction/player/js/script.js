a7c05cd6d45779680efd3b677d2e06dcb6ce9fda
Alexander MacKinnon <alexmackinnon55@gmail.com>
2023-04-14 00:03:42 -0400

    Fix asset link error

--- Projet 3 - Playful Interaction/player/js/script.js
+++ Projet 3 - Playful Interaction/player/js/script.js
@@ -89,8 +89,8 @@ let colors = {
 
 /* ------- Setup Function ------- */
 function preload() {
-  brutalityRegFont = loadFont("/assets/fonts/brutality.regular.ttf");
-  brutalityExtraFont = loadFont("/assets/fonts/brutality.extra.ttf");
+  brutalityRegFont = loadFont("../assets/fonts/brutality.regular.ttf");
+  brutalityExtraFont = loadFont("../assets/fonts/brutality.extra.ttf");
 }
 
 /* ------- Setup Function ------- */
